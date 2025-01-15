5564fcfb0fa5354b57d5499aa7d522693d1f4d5d
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-01-15 18:33:13 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,9 +99,9 @@ if uploaded_file is not None:
         
         plotNetwork(df, node_scaler, node_shape)
         
-        st.write()
-        st.write()
-        st.dataframe(df, width = 777)
+        #st.write()
+        #st.write()
+        #st.dataframe(df, width = 777)
         
         
                 
