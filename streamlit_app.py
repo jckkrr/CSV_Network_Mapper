2073cfab8a354826d2511733712a7cb7857679d7
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-01-15 18:46:03 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def plotNetwork(df, node_scaler, node_shape):
     
     components.html(
         HtmlFile.read(), 
-        height = 1000, 
+        height = 777, 
         width = 777
     )
     
