0a219a5aa1bc8128f9c202795e046f912cff2fa9
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-01-17 19:48:13 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 from pyvis.network import Network
 import pyvis
 
-css = 'h1 {font-family: "Manrope", sans-serif;} body, html, p, [class*="css"] {font-family: "Inter", sans-serif;} '
+css = 'body, html, p, [class*="css"] {font-family: "Inter", sans-serif;} '
 
 st.markdown( f'<style>{css}</style>' , unsafe_allow_html= True)
     
