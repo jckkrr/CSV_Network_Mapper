d46af4e742e8e4a5634f72c59e51af60e56e24c7
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-01-17 19:07:12 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ def plotNetwork(df, node_scaler, node_shape):
     )
     
     downloadable_html = g.save_graph(f'temp.html')
-    downloadable_html = g.generate_html(g.save_graph(f'temp.html'))
+    #downloadable_html = g.generate_html(g.save_graph(f'temp.html'))
+    downloadable_html = g.generate_html(HtmlFile)
     #downloadable_html = downloadable_html.replace('height: 500px', 'height: 100%')   
     #downloadable_html = downloadable_html.replace('border: 1px solid lightgray', 'border: 0px solid lightgray') # removes border that otherwise appears
     #downloadable_html = downloadable_html.replace('background-color:rgba(200,200,200,0.8)', 'background: linear-gradient(to bottom right, #99ffcc 0%, #ffffcc 100%);')
