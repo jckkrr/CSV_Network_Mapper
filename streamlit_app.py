e89162773927a94869f810d7d6d00d1754602c47
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-01-17 19:14:20 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,27 +48,29 @@ def plotNetwork(df, node_scaler, node_shape):
     g.save_graph(f'temp.html')
     HtmlFile = open(f'temp.html', 'r', encoding='utf-8')
     
+    source_code = HtmlFile.read()
+    
     components.html(
-        HtmlFile.read(), 
+        source_code, 
         height = 550, 
         width = 777
     )
     
     #downloadable_html = g.save_graph(f'temp.html')
     #st.write(type(downloadable_html))
-    x = g.save_graph(f'temp.html')
-    downloadable_html = g.generate_html(x)
+     
+    #downloadable_html = g.generate_html(x)
     #st.write(type(downloadable_html))
     #downloadable_html = downloadable_html.replace('height: 500px', 'height: 100%')   
     #downloadable_html = downloadable_html.replace('border: 1px solid lightgray', 'border: 0px solid lightgray') # removes border that otherwise appears
     #downloadable_html = downloadable_html.replace('background-color:rgba(200,200,200,0.8)', 'background: linear-gradient(to bottom right, #99ffcc 0%, #ffffcc 100%);')
     
-    #st.download_button(
-    #    label = "For easier analysis, download as HTML",
-    #    data = downloadable_html,
-    #    file_name = "downloadable_html.html",
-    #    mime = "application/octet-stream",
-    #)
+    st.download_button(
+        label = "For easier analysis, download as HTML",
+        data = source_code,
+        file_name = "downloadable_html.html",
+        mime = "application/octet-stream",
+    )
     
 ### MAIN SCRIPT ################################################
 
